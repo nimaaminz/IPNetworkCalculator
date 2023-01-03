13f45b67b22ac9b9314945c590ef20ef8c338a9d
Nima <nimaaminz19900@gmail.com>
2023-01-03 10:49:25 +0330

    Hello First Commit

---
+++ NetworkExtractor/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace NetworkExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.input_cidr = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_network_ip = new System.Windows.Forms.Label();
+            this.lbl_first_ip = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_last_ip = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl_boradcast = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txt_input_ip = new System.Windows.Forms.TextBox();
+            this.txt_input_mask = new System.Windows.Forms.TextBox();
+            this.lbl_host_count = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.input_cidr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.ForeColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(0, 250);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A.Aminzadeh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CIDR";
+            // 
+            // input_cidr
+            // 
+            this.input_cidr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.input_cidr.Location = new System.Drawing.Point(21, 154);
+            this.input_cidr.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.input_cidr.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.input_cidr.Name = "input_cidr";
+            this.input_cidr.Size = new System.Drawing.Size(76, 21);
+            this.input_cidr.TabIndex = 5;
+            this.input_cidr.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.input_cidr.ValueChanged += new System.EventHandler(this.input_cidr_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mask";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Network IP";
+            // 
+            // lbl_network_ip
+            // 
+            this.lbl_network_ip.AutoSize = true;
+            this.lbl_network_ip.Location = new System.Drawing.Point(289, 35);
+            this.lbl_network_ip.Name = "lbl_network_ip";
+            this.lbl_network_ip.Size = new System.Drawing.Size(25, 13);
+            this.lbl_network_ip.TabIndex = 9;
+            this.lbl_network_ip.Text = ". . . ";
+            // 
+            // lbl_first_ip
+            // 
+            this.lbl_first_ip.AutoSize = true;
+            this.lbl_first_ip.Location = new System.Drawing.Point(289, 85);
+            this.lbl_first_ip.Name = "lbl_first_ip";
+            this.lbl_first_ip.Size = new System.Drawing.Size(28, 13);
+            this.lbl_first_ip.TabIndex = 11;
+            this.lbl_first_ip.Text = ". . . .";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(289, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "First IP";
+            // 
+            // lbl_last_ip
+            // 
+            this.lbl_last_ip.AutoSize = true;
+            this.lbl_last_ip.Location = new System.Drawing.Point(288, 136);
+            this.lbl_last_ip.Name = "lbl_last_ip";
+            this.lbl_last_ip.Size = new System.Drawing.Size(28, 13);
+            this.lbl_last_ip.TabIndex = 13;
+            this.lbl_last_ip.Text = ". . . .";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(288, 110);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Last IP";
+            // 
+            // lbl_boradcast
+            // 
+            this.lbl_boradcast.AutoSize = true;
+            this.lbl_boradcast.Location = new System.Drawing.Point(287, 186);
+            this.lbl_boradcast.Name = "lbl_boradcast";
+            this.lbl_boradcast.Size = new System.Drawing.Size(28, 13);
+            this.lbl_boradcast.TabIndex = 15;
+            this.lbl_boradcast.Text = ". . . .";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(287, 160);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 13);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Broadcast";
+            // 
+            // txt_input_ip
+            // 
+            this.txt_input_ip.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txt_input_ip.Location = new System.Drawing.Point(22, 52);
+            this.txt_input_ip.Name = "txt_input_ip";
+            this.txt_input_ip.Size = new System.Drawing.Size(108, 21);
+            this.txt_input_ip.TabIndex = 17;
+            this.txt_input_ip.Text = "192.168.0.";
+            this.txt_input_ip.TextChanged += new System.EventHandler(this.txt_input_ip_TextChanged);
+            // 
+            // txt_input_mask
+            // 
+            this.txt_input_mask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txt_input_mask.Location = new System.Drawing.Point(22, 102);
+            this.txt_input_mask.Name = "txt_input_mask";
+            this.txt_input_mask.Size = new System.Drawing.Size(108, 21);
+            this.txt_input_mask.TabIndex = 18;
+            this.txt_input_mask.TextChanged += new System.EventHandler(this.txt_input_mask_TextChanged);
+            // 
+            // lbl_host_count
+            // 
+            this.lbl_host_count.AutoSize = true;
+            this.lbl_host_count.Location = new System.Drawing.Point(289, 232);
+            this.lbl_host_count.Name = "lbl_host_count";
+            this.lbl_host_count.Size = new System.Drawing.Size(28, 13);
+            this.lbl_host_count.TabIndex = 20;
+            this.lbl_host_count.Text = ". . . .";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(289, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Hosts Count";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::NetworkExtractor.Properties.Resources.A;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(465, 267);
+            this.Controls.Add(this.lbl_host_count);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_input_mask);
+            this.Controls.Add(this.txt_input_ip);
+            this.Controls.Add(this.lbl_boradcast);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lbl_last_ip);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbl_first_ip);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl_network_ip);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.input_cidr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.input_cidr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown input_cidr;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_network_ip;
+        private System.Windows.Forms.Label lbl_first_ip;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_last_ip;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbl_boradcast;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txt_input_ip;
+        private System.Windows.Forms.TextBox txt_input_mask;
+        private System.Windows.Forms.Label lbl_host_count;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ NetworkExtractor/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkExtractor
+{
+    public partial class Form1 : Form
+    {
+
+        private bool VALID_INPUTS = false;
+
+        private bool MASK_ACTIVE = false;
+        private bool CIDR_ACTIVE = false;
+
+        private bool PROGRAM_MASK_CHANGE_TEXT = false;
+        private bool PROGRAM_CIDR_CHANGE_TEXT = false;
+
+        private int CIDR_INPUT = 0;
+
+        private uint INPUT_SUBNET_MASK = 0;
+        private uint INPUT_IP = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void txt_input_ip_TextChanged(object sender, EventArgs e)
+        {
+            string pattern = @"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+            if (Regex.IsMatch(txt_input_ip.Text, pattern))
+            {
+
+                IPAddress ip = null;
+
+                if (IPAddress.TryParse(txt_input_ip.Text, out ip))
+                {
+                    VALID_INPUTS = true;
+
+
+                    string[] octeds = txt_input_ip.Text.Split('.');
+
+                    uint ipv4 = (uint)
+                    (
+                        int.Parse(octeds[0]) * Math.Pow(256, 3) +
+                        int.Parse(octeds[1]) * Math.Pow(256, 2) +
+                        int.Parse(octeds[2]) * 256 +
+                        int.Parse(octeds[3])
+                    );
+
+
+                    INPUT_IP = ipv4;
+
+                    calculate();
+
+                }
+                else
+                {
+                    VALID_INPUTS = false;
+                }
+            }
+            else
+            {
+                VALID_INPUTS = false;
+            }
+        }
+
+        private void txt_input_mask_TextChanged(object sender, EventArgs e)
+        {
+            string pattern = @"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+            if (!PROGRAM_MASK_CHANGE_TEXT)
+            {
+                if (Regex.IsMatch(txt_input_mask.Text, pattern))
+                {
+                    MASK_ACTIVE = true;
+                    CIDR_ACTIVE = false;
+
+                    string[] octeds = txt_input_mask.Text.Split('.');
+
+                    uint ipv4 = (uint)
+                    (
+                        uint.Parse(octeds[0]) * Math.Pow(256, 3) +
+                        uint.Parse(octeds[1]) * Math.Pow(256, 2) +
+                        uint.Parse(octeds[2]) * 256 +
+                        uint.Parse(octeds[3])
+                    );
+
+                    CIDR_INPUT = 0;
+                    INPUT_SUBNET_MASK = ipv4;
+                    calculate();
+
+                }
+            }
+            else
+            {
+                PROGRAM_MASK_CHANGE_TEXT = false;
+            }
+        }
+
+
+        private void input_cidr_ValueChanged(object sender, EventArgs e)
+        {
+            if (!PROGRAM_CIDR_CHANGE_TEXT)
+            {
+                MASK_ACTIVE = false;
+                CIDR_ACTIVE = true;
+
+                CIDR_INPUT = int.Parse(input_cidr.Value.ToString());
+                INPUT_SUBNET_MASK = 0;
+
+                calculate();
+            }
+            else
+            {
+                PROGRAM_CIDR_CHANGE_TEXT = false;
+            }
+        }
+
+
+        private void calculate()
+        {
+            uint network_address = 0;
+            uint subnet_mask = 0;
+
+            if (CIDR_ACTIVE)
+            {
+                //int floor_8bit = (int)Math.Floor(decimal.Parse((CIDR_INPUT / 8).ToString())) ; 
+                int floor_8bit = (int)Math.Floor((decimal)(CIDR_INPUT / 8));
+                int after_pt = (CIDR_INPUT - (floor_8bit * 8));
+
+                for (int i = 0; i < floor_8bit; i++)
+                {
+                    uint oc = ((uint)255 << ((3 - i) * 8));
+                    subnet_mask |= oc;
+                }
+
+                for (int i = 0; i < after_pt; i++)
+                {
+                    subnet_mask |= ((uint)1 << ((3 - floor_8bit) * 8) + (8 - after_pt) + i);
+                }
+
+
+
+            }
+
+            if (MASK_ACTIVE)
+            {
+                subnet_mask = INPUT_SUBNET_MASK;
+
+                CIDR_INPUT = 0;
+
+                for (int i = 0; i < 32; i++)
+                {
+                    if ((subnet_mask & (1<<i)) == 0)
+                    {
+                        CIDR_INPUT++ ; 
+                    }
+                    else
+                    {
+                        break ; 
+                    }
+                }
+
+                CIDR_INPUT = 32 - CIDR_INPUT ;
+
+            }
+
+            network_address = subnet_mask & (uint)INPUT_IP;
+
+            uint first_ip = network_address + 1;
+
+            uint broad_cast = 0;
+
+            for (int j = 0; j < 32 - CIDR_INPUT; j++)
+            {
+                broad_cast |= (uint)1 << j;
+            }
+
+            broad_cast |= network_address ; 
+
+
+
+            uint last_ip = broad_cast - 1;
+
+
+            // print network address . .  . 
+            byte[] ip_net = new byte[4];
+            ip_net = BitConverter.GetBytes(network_address);
+
+            byte[] f_ip = new byte[4];
+            f_ip = BitConverter.GetBytes(first_ip);
+
+            byte[] br_ip = new byte[4];
+            br_ip = BitConverter.GetBytes(broad_cast);
+
+            byte[] ls_ip = new byte[4];
+            ls_ip = BitConverter.GetBytes(last_ip);
+
+            byte[] sub = new byte[4];
+            sub = BitConverter.GetBytes(subnet_mask);
+
+
+
+            char class_range = ip_net[3] >= 0 && ip_net[3] <= 127 ?
+                'A'
+                :
+                (
+                    ip_net[3] >= 128 && ip_net[3] <= 191 ?
+                    'B'
+                    :
+                    'C'
+                );
+
+
+            Task.Run(() =>
+            {
+                switch (class_range)
+                {
+                    case 'A':
+                        this.BackgroundImage = NetworkExtractor.Properties.Resources.A;
+                        break;
+                    case 'B':
+                        this.BackgroundImage = NetworkExtractor.Properties.Resources.B;
+                        break;
+                    case 'C':
+                        this.BackgroundImage = NetworkExtractor.Properties.Resources.C;
+                        break;
+                }
+
+            });
+
+
+            lbl_network_ip.Text = ip_net[3] + "." + ip_net[2] + "." + ip_net[1] + "." + ip_net[0];
+            lbl_first_ip.Text = f_ip[3] + "." + f_ip[2] + "." + f_ip[1] + "." + f_ip[0];
+            lbl_boradcast.Text = br_ip[3] + "." + br_ip[2] + "." + br_ip[1] + "." + br_ip[0];
+            lbl_last_ip.Text = ls_ip[3] + "." + ls_ip[2] + "." + ls_ip[1] + "." + ls_ip[0];
+
+            if (MASK_ACTIVE)
+            {
+                PROGRAM_CIDR_CHANGE_TEXT = true ; 
+                input_cidr.Value = Math.Min(input_cidr.Maximum , Math.Max(CIDR_INPUT , input_cidr.Minimum)); 
+            }
+
+            lbl_host_count.Text = (Math.Pow(2, 32 - CIDR_INPUT) - 2).ToString();
+            if (CIDR_ACTIVE)
+            {
+                PROGRAM_MASK_CHANGE_TEXT = true;
+                txt_input_mask.Text = sub[3] + "." + sub[2] + "." + sub[1] + "." + sub[0];
+            }
+
+
+        }
+
+    }
+}
